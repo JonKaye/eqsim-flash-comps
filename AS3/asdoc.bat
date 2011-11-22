2ea295c1cfee68b3db196106b0fa3578d5dc6795
Jonathan Kaye <jmk@eqsim.com>
2011-11-22 15:10:38 -0500

    generated docs directly from current folder

---
+++ AS3/asdoc.bat
@@ -0,0 +1,5 @@
+cls
+set path=C:\Program Files (x86)\Adobe\Adobe Flash Builder 4.5\sdks\4.5.1\bin
+set JAVA_HOME="C:\Program Files (x86)\Java\jre1.5.0_22"
+asdoc.exe -source-path "C:\Users\Jonathan\Projects\EqSim-State-Machine-and-Components\AS3" -doc-sources "C:\Users\Jonathan\Projects\EqSim-State-Machine-and-Components\AS3\com" -window-title "EqSim Hierarchical State Engine and Equipment Interface Components" -main-title "EqSim Hierarchical State Engine and Equipment Interface Components" -footer "Copyright 2004-2011<br/><a href=\"http://www.eqsim.com\" target=\"_blank\">www.eqsim.com</a>" -output "C:\Users\Jonathan\Projects\EqSim-State-Machine-and-Components\AS3\docs" -library-path "C:\Users\Jonathan\Projects\EqSim-State-Machine-and-Components\AS3\fl-shim.swc" -package-description-file myPackages.xml
+pause
